6d38cf047b36f3866175e430f8a674a533c72f2f
yunsoo <04pys@naver.com>
2021-07-31 22:40:14 +0900

    각 액티비티의 list/recyclerview 멤버 변수화

--- app/src/main/java/com/example/recyclerview_0731/Recyclerview_base.java
+++ app/src/main/java/com/example/recyclerview_0731/Recyclerview_base.java
@@ -1,6 +1,7 @@
 package com.example.recyclerview_0731;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class Recyclerview_base extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview_base);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewbase);
     }
 }
